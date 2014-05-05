5d31df6299ff50124f68fe2f641d97e3e1109ee2
Catela <Catela@Kataventos.lan>
2014-05-05 18:23:31 +0100

    Adaptation notice.

--- KV_Team_OSD/KV_Team_OSD.ino
+++ KV_Team_OSD/KV_Team_OSD.ino
@@ -1,22 +1,12 @@
 /*
-KV Team OSD V2.3 April 2014
-http://code.google.com/p/rush-osd-development/
- Copyright (C) January 2014 (KV Team OSD)
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- any later version. see http://www.gnu.org/licenses/
+ KV Team OSD V2.3 May 2014
+ http://code.google.com/p/rush-osd-development/
+
+ This work is a Software adaptation of Rushduino OSD http://code.google.com/p/rushduino-osd/ 
+ 
+ Work released under the Creative Commons Attribution NonCommercial ShareAlike 3.0 Unported License.
+ http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
 */
-
-//This work is based on the earlier work done by Jean Gabriel Maurice known as Rushduino. http://code.google.com/p/rushduino-osd/
-
-
-              /*********************************************************************/
-              /*                           KV_OSD_Team                             */
-              /*                                                                   */
-              /*    KATAVENTOS                ITAIN                 CARLONB        */
-              /*     POWER67                 LIAM2317             NEVERLANDED      */
-              /*********************************************************************/
 
 
 #include <avr/pgmspace.h>
